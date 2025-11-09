eaabfff81c39e76ac63577e9a6c02cc59111d0b3
Ronith Sharmila <107947701+heyronith@users.noreply.github.com>
2025-11-09 10:37:53 -0600

    Change video preload attribute to 'auto'

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -213,7 +213,7 @@ const Hero = () => {
                             muted
                             playsInline
                             autoPlay
-                            preload="metadata"
+                            preload="auto"
                             loop
                             className="w-full h-full object-cover group-hover:scale-[1.02] transition-transform duration-700"
                           >
